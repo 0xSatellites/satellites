87242d00cd7ac035157a7179f10140850f492a6c
ookimaki <noreply@ookimaki.com>
2019-01-26 08:45:30 +0000

    remove nuxt

--- src/api/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.header('Content-Type', 'text/plain; charset=utf-8')
-  res.send("This is api for mch market place");
-});
-
-module.exports = router;
--- src/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<div>
-TBD
-</div>
-</template>

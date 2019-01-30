8bb6a53ce3998b504b23aa7849cda4cad2bac3a5
ookimaki <noreply@ookimaki.com>
2019-01-30 06:59:20 +0000

    add mche image

--- scripts/image.js
+++ scripts/image.js
@@ -12,13 +12,20 @@ const db = admin.firestore()
 const UUID = require("uuid-v4");
 
 const mchh_path = "https://www.origin.sand.mch.djty.co/images/heroes/2000/"
-const mchh_range_5 = [5001, 5013]
-const mchh_range_4 = [4001, 4020]
-const mchh_range_3 = [3001, 3018]
+const mche_path = "https://www.origin.sand.mch.djty.co/images/extensions/2000/"
 const mchh_range_2 = [2001, 2009]
+const mchh_range_3 = [3001, 3018]
+const mchh_range_4 = [4001, 4020]
+const mchh_range_5 = [5001, 5013]
+
+const mche_range_1 = [1001, 1014]
+const mche_range_2 = [2001, 2014]
+const mche_range_3 = [3001, 3014]
+const mche_range_4 = [4001, 4014]
+const mche_range_5 = [5001, 5014]
 
 //mchh()
-
+//mche()
 
 async function mchh(){
   var list = []
@@ -34,17 +41,38 @@ async function mchh(){
   for(var i=mchh_range_2[0]; i<=mchh_range_2[1]; i++){
     list.push(i)
   }
-  console.log("start bulk: " + list)
   bulk(mchh_path, list, "png", "mchh")
 }
 
+async function mche(){
+  var list = []
+  for(var i=mche_range_5[0]; i<=mche_range_5[1]; i++){
+    list.push(i)
+  }
+  for(var i=mche_range_4[0]; i<=mche_range_4[1]; i++){
+    list.push(i)
+  }
+  for(var i=mche_range_3[0]; i<=mche_range_3[1]; i++){
+    list.push(i)
+  }
+  for(var i=mche_range_2[0]; i<=mche_range_2[1]; i++){
+    list.push(i)
+  }
+  for(var i=mche_range_1[0]; i<=mche_range_1[1]; i++){
+    list.push(i)
+  }
+  bulk(mche_path, list, "png", "mche")
+}
+
 async function bulk(path, list, format, category){
+  console.log("start bulk: " + list)
   for(var i=0; i<list.length; i++){
     await single(path, list[i], format, category)
   }
 }
 
 async function single(path, i, format, category){
+  console.log("start single: " + path, i, format, category)
   var uuid = UUID();
   var data = await bucket.upload(path + i + "." + format,{
     destination: "/" + category + "/" + i + ".png",
@@ -61,5 +89,5 @@ async function single(path, i, format, category){
     url:"https://firebasestorage.googleapis.com/v0/b/" + bucket.name + "/o/" + encodeURIComponent(file.name) + "?alt=media&token=" + uuid,
   }
   db.collection('image').doc(category + '_' + i.toString()).set(record)
-  console.log("done: " + path, i, format, category)
+
 }

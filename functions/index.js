5f47c59ab193074951447cdc3f34e4b9e85f6ac5
ookimaki <noreply@ookimaki.com>
2019-01-30 02:27:36 +0000

    add cloud function mockup

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const serviceAccount = require('../.serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore()
+
+exports.order = functions.https.onRequest((req, res) => {
+  const path = req.path.split('/')
+  const order = path[2]
+  console.log()
+  db.collection('order').doc(order).get().then((doc) => {
+    console.log(doc.data())
+    //res.status(200).end(doc.data())
+  }).catch(err => {
+    console.log(err)
+    //res.status(500).end(err)
+  })
+});

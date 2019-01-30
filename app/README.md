1d5304607004df5e5a8c1721ff2fceb96b3c97d1
ookimaki <noreply@ookimaki.com>
2019-01-30 12:10:20 +0000

    minify app

--- app/README.md
+++ app/README.md
@@ -6,17 +6,17 @@
 
 ``` bash
 # install dependencies
-$ yarn install
+$ npm install
 
 # serve with hot reload at localhost:3000
-$ yarn run dev
+$ npm run dev
 
 # build for production and launch server
-$ yarn run build
-$ yarn start
+$ npm run build
+$ npm start
 
 # generate static project
-$ yarn run generate
+$ npm run generate
 ```
 
 For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -7,24 +7,17 @@ module.exports = {
   /*
   ** Headers of the page
   */
- head: {
-  title: pkg.name,
-  meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { hid: 'description', name: 'description', content: pkg.description },
-    { hid: 'og:site_name', property: 'og:site_name', content: pkg.name },
-    { hid: 'og:type', property: 'og:type', content: 'website' },
-    { hid: 'og:url', property: 'og:url', content: 'https://bazaaar.io' },
-    { hid: 'og:title', property: 'og:title', content: pkg.name },
-    { hid: 'og:description', property: 'og:description', content: pkg.description },
-    { hid: 'og:image', property: 'og:image', content: '' },
-    { hid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image'}
-  ],
-  link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-  ]
-},
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
 
   /*
   ** Customize the progress-bar color
@@ -34,10 +27,10 @@ module.exports = {
   /*
   ** Global CSS
   */
- css: [
-  "@/assets/style/style.css",
-  "@/assets/style/style_sp.css"
-],
+  css: [
+    "@/assets/style/style.css",
+    "@/assets/style/style_sp.css"
+  ],
 
   /*
   ** Plugins to load before mounting the App
@@ -60,7 +53,7 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-
+      
     }
   }
 }

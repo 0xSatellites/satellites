e0c9441f0d1200c3051492072be76f6825465489
ookimaki <noreply@ookimaki.com>
2019-02-01 02:20:48 +0000

    remove sandbox

--- functions/lib/canvas.js
+++ functions/lib/canvas.js
@@ -1,9 +1,20 @@
+const fs = require('fs')
 const Canvas = require('canvas')
 Canvas.registerFont(__dirname  + '/assets/fonts/NotoSansJP-Regular.otf', { family: 'Noto Sans JP' })
 Canvas.registerFont(__dirname  + '/assets/fonts/NotoSansJP-Bold.otf', { family: 'Noto Sans JP Bold', weight: 'bold'})
 
 const draw = (order, status, message) => {
-    console.log("ok")
+
+    var canvas = Canvas.createCanvas(1200,630)
+    var context = canvas.getContext('2d')
+    var loadedImg=[]
+    var bgImg = new Canvas.Image()
+    bgImg.src = __dirname + '/assets/img/bg.png'
+    console.log(__dirname + '/assets/img/bg.png')
+    bgImg.onload = function() {
+      console.log("ok")
+      context.drawImage(bgImg, 0, 0)
+    }
 
 }
 

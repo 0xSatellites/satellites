842bbba390a842aa13617a0f19bf5cafe91eefdb
Kohei Tanaka <kohei69@gmail.com>
2019-02-13 18:19:38 +0900

    #35

---
+++ app/pages/_lang/ck/order/_hash.vue
@@ -0,0 +1,230 @@
+<template>
+<div>
+    <section class="l-information">
+      <div ><img class="ogpimg" :src="order.ogp" width=100%></div>
+      <div class="l-information__frame">
+        <div class="l-information__name">{{ order.metadata.name}} / Gen.{{ order.metadata.generation}}</div>
+        <div class="l-information__txt"># {{ order.metadata.id}}</div>
+        <div class="l-information__txt">Crypto Kitties</div>
+
+        <!-- <ul class="l-information__data">
+        <li><span class="l-information__rarity l-item__rarity--5">★★★★★</span>{{ order.metadata.fancy_type}}</li>
+        </ul> -->
+        <!-- <ul class="l-information__data">
+        <li><strong>HP：</strong> {{ order.metadata.attributes.hp}}</li>
+        <li><strong>PHY：</strong> {{ order.metadata.attributes.phy}}</li>
+        <li><strong>INT：</strong> {{ order.metadata.attributes.int}}</li>
+        <li><strong>AGI：</strong> {{ order.metadata.attributes.agi}}</li>
+        </ul> -->
+        <!-- <ul class="l-information__data">
+
+        <li><span class="l-information__skill--type">Active</span>
+          【{{ order.metadata.active_skill.name.ja}}】<br>
+          {{ order.metadata.active_skill.description.ja}}
+        </li>
+        <li><span class="l-information__skill--type">Passive</span>
+          【{{ order.metadata.passive_skill.name.ja}}】<br>
+          {{ order.metadata.passive_skill.description.ja}}
+        </li>
+        </ul> -->
+        <v-form v-model="valid">
+        <div class="l-information__action">
+          <v-btn v-if="order.status == '出品中'"
+          class="l-information__action__btn"
+          :disabled="!valid || loading"
+          color="#3498db"
+          large
+          @click="purchase"
+          value="purchase"
+          >購入する
+           <v-progress-circular size=16 class="ma-2" v-if="loading"
+              indeterminate
+            ></v-progress-circular>
+          </v-btn>
+      </div>
+       <v-flex center>
+            <v-checkbox
+              class="center"
+              v-model="checkbox"
+              :rules="[v => !!v || '']"
+              label="利用規約に同意する"
+              required
+            ></v-checkbox>
+        </v-flex>
+        </v-form>
+    </div>
+    <div class="l-information__action__btn">
+       <a :href="'https://twitter.com/share?url=https://bazaaar.io/ck/order/' + order.hash +
+        '&text=' + '出品されました! '+ order.metadata.name  + '/ Gen.' + order.metadata.generation +
+        '&hashtags=bazaaar, バザール, CryptoKitties'" class="twitter-share-button" data-size="large" data-show-count="false" target=”_blank”>
+        Tweetする
+        </a>
+        <!-- <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
+    </div>
+    </section>
+    <section class="c-index c-index--recommend">
+
+        <h2 class="c-index__title">おすすめアセット</h2>
+        <ul>
+        <li v-for="(recommend, i ) in recommend" :key="i">
+        <nuxt-link :to="'/order/'+ recommend.hash" class="c-card">
+        <div class="c-card__label c-card__label__rarity--1">★1</div>
+        <div class="c-card__img"><img :src="recommend.metadata.image_url" alt=""></div>
+        <div class="c-card__name">{{recommend.metadata.hero_type.name.ja}} / LV.{{ recommend.metadata.attributes.lv }} </div>
+        <div class="c-card__txt">#{{recommend.id}}</div>
+        <div class="c-card__txt">My Crypto Heores</div>
+        <div class="c-card__eth">{{recommend.price / 1000000000000000000}} ETH</div>
+        </nuxt-link>
+        </li>
+        </ul>
+    </section>
+    <div>
+
+    </div>
+    <transition name="modal" v-if="modal">
+        <div class="l-modal">
+
+            <div class="l-modal__frame">
+
+                <div class="l-modal__icon"><img src="~/assets/img/modal/icon.svg" alt=""></div>
+                <div class="l-modal__title">購入処理完了しました！</div>
+
+                <div class="l-modal__og">
+                    <div id="modalImg">
+                      <img  :src="order.ogp" alt=""  width="85%">
+                    </div>
+                </div>
+                <div class="l-modal__txt">トランザクションハッシュ</div>
+                <div class="l-modal__txt"><nuxt-link :to="'https://etherscan.io/tx/' + hash">Ethescan</nuxt-link></div>
+
+                <div class="l-modal__close" @click="closeModal">
+                  <div class="l-modal__close__icon" ></div>
+                  <div class="l-modal__close__txt u-obj--sp">閉じる</div>
+                </div>
+
+            </div>
+
+        </div>
+      </transition>
+
+  </div>
+</template>
+
+<script>
+import client from '~/plugins/ethereum-client'
+import firestore from '~/plugins/firestore'
+
+const config = require('../../../../config.json')
+
+export default {
+  head() {
+    var order = this.order
+    return {
+      meta:
+      [{ hid: 'og:image', property: 'og:image', content: order.ogp }]
+    }
+  },
+  data() {
+    return {
+      loading:false,
+      valid: true,
+      checkbox: false,
+      modal: false,
+      hash: "",
+      }
+  },
+  async asyncData({ store, params }) {
+    const order = await firestore.doc('order', params.hash)
+    await store.dispatch('order/setOrder', order)
+
+    // const recommend2 = await firestore.docs('order','status', '==', '出品中', 'metadata.attributes.rarity' , '==', order.metadata.attributes.rarity, )
+    // recommend2.sort((a, b) => {
+    //       if (a.price < b.price) return -1;
+    //       if (a.price > b.price) return 1;
+    //       return 0;
+    //     });
+    // const recommend = recommend2.slice(0,4)
+    // await store.dispatch('recommend/setRecommend', recommend)
+  },
+  mounted: async function() {
+    const store = this.$store
+    if (typeof web3 != 'undefined') {
+      if (!client.account.address) {
+        //initialize web3 client
+        const account = await client.activate(web3.currentProvider)
+        store.dispatch('account/setAccount', account)
+      }
+    }
+  },
+  computed: {
+    account() {
+      return this.$store.getters['account/account']
+    },
+    order() {
+      return this.$store.getters['order/order']
+    },
+    recommend(){
+      return this.$store.getters['recommend/recommend']
+    }
+  },
+  methods: {
+    async purchase() {
+      const account = this.account
+      const order = this.order
+
+      await client.contract.bazaaar_v1.methods
+        .orderMatch_(
+          [
+            order.proxy,
+            order.maker,
+            order.taker,
+            order.creatorRoyaltyRecipient,
+            order.asset,
+            order.maker
+          ],
+          [
+            order.id,
+            order.price,
+            order.nonce,
+            order.salt,
+            order.expiration,
+            order.creatorRoyaltyRatio,
+            order.referralRatio
+          ],
+          order.v,
+          order.r,
+          order.s,
+        )
+        .send({ from: account.address, value: order.price })
+        .on('transactionHash', function(hash) {
+          console.log(hash)
+          this.hash = hash
+          this.modal = true
+        })
+    },
+    openModal() {
+      this.modal = true
+    },
+    closeModal() {
+      this.modal = false
+    },
+  }
+}
+</script>
+<style scoped>
+.twitter-share-button {
+text-decoration: none;
+color: white;
+}
+
+.share{
+  max-width: 100px;
+  text-align: center;
+  padding: 10px 0;
+  margin: auto;
+}
+.ogpimg{
+  
+}
+</style>
+
---
+++ app/pages/_lang/index.vue
@@ -0,0 +1,95 @@
+<template>
+<div>
+    <section class="l-visual">
+        <div class="l-visual__bg"><img src="~/assets/img/top_visual/bg_pc.png" alt="" class="u-obj--pc"><img src="~/assets/img/top_visual/bg_sp.png" alt="" class="u-obj--sp"></div>
+
+        <div class="l-visual__txts">
+        <h1 class="l-visual__txts__title"><img src="~/assets/img/top_visual/title_pc.png" alt="誰でもカンタン デジタルアセットの取引マッチングサービス - bazaaar.io" class="u-obj--pc"><img src="~/assets/img/top_visual/title_sp.png" alt="" class="u-obj--sp"></h1>
+        </div>
+
+        <div class="l-visual__img"><img src="~/assets/img/top_visual/illust.png" alt=""></div>
+
+        <div class="l-about">
+        <ul>
+        <li>
+        <h2 class="l-about__title">{{ $t('index.subTitle1') }}</h2>
+        <p class="l-about__txt">{{ $t('index.subDiscription1') }}</p>
+        </li>
+        <li>
+        <h2 class="l-about__title">{{ $t('index.subTitle2') }}</h2>
+        <p class="l-about__txt">{{ $t('index.subDiscription2') }}</p>
+        </li>
+        <li>
+        <h2 class="l-about__title">{{ $t('index.subTitle3') }}</h2>
+        <p class="l-about__txt">{{ $t('index.subDiscription3') }}</p>
+        </li>
+        </ul>
+        </div>
+    </section>
+    <section class="c-index c-index--recommend">
+
+        <h2 class="c-index__title">新着アセット</h2>
+        <ul>
+        <li>
+        <a href="#" class="c-card">
+        <div class="c-card__label c-card__label__rarity--1">★1</div>
+        <div class="c-card__img"><img src="~/assets/img/card/sample_img.png" alt=""></div>
+        <div class="c-card__name">Masamune Date / LV.99</div>
+        <div class="c-card__txt">#30010206</div>
+        <div class="c-card__txt">My Crypto Heores</div>
+        <div class="c-card__eth">Ξ 0.45</div>
+        </a>
+        </li>
+        <li>
+        <a href="#" class="c-card">
+        <div class="c-card__label c-card__label__rarity--2">★2</div>
+        <div class="c-card__img"><img src="~/assets/img/card/sample_img.png" alt=""></div>
+        <div class="c-card__name">Masamune Date / LV.99</div>
+        <div class="c-card__txt">#30010206</div>
+        <div class="c-card__txt">My Crypto Heores</div>
+        <div class="c-card__eth">Ξ 0.45</div>
+        </a>
+        </li>
+        <li>
+        <a href="#" class="c-card">
+        <div class="c-card__label c-card__label__rarity--3">★3</div>
+        <div class="c-card__img"><img src="~/assets/img/card/sample_img.png" alt=""></div>
+        <div class="c-card__name">Masamune Date / LV.99</div>
+        <div class="c-card__txt">#30010206</div>
+        <div class="c-card__txt">My Crypto Heores</div>
+        <div class="c-card__eth">Ξ 0.45</div>
+        </a>
+        </li>
+        <li>
+        <a href="#" class="c-card">
+        <div class="c-card__label c-card__label__rarity--4">★4</div>
+        <div class="c-card__img"><img src="~/assets/img/card/sample_img.png" alt=""></div>
+        <div class="c-card__name">Masamune Date / LV.99</div>
+        <div class="c-card__txt">#30010206</div>
+        <div class="c-card__txt">My Crypto Heores</div>
+        <div class="c-card__eth">Ξ 0.45</div>
+        </a>
+        </li>
+        </ul>
+    </section>
+    <section class="c-index">
+        <h2 class="c-index__title">取り扱いアセット</h2>
+    </section>
+    <section class="c-index">
+        <h2 class="c-index__title">パートナー</h2>
+    </section>
+</div>
+</template>
+
+<script>
+
+export default {
+
+head() {
+    return { title: this.$t('meta.titile') }
+  }
+}
+</script>
+
+<style>
+</style>

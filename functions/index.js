33eec3b9c2ab2935e5fe8cbc45381cf571a84568
ookimaki <noreply@ookimaki.com>
2019-02-20 14:43:03 +0000

    add cd

--- functions/index.js
+++ functions/index.js
@@ -33,7 +33,7 @@ const bazaaar_v1 = new web3.eth.Contract(
 )
 
 exports.order = functions.region('asia-northeast1').https.onCall(async (params, context) => {
-  console.log("test!!!")
+  console.log("test!!!!")
   const data = params.order
   const hash = await bazaaar_v1.methods
     .requireValidOrder_(

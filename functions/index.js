b100d87dbf01cbf0d388c97303b1b71c4e7589ca
ookimaki <noreply@ookimaki.com>
2019-02-21 08:55:41 +0000

    add

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const config = require('./config.json')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
-const project = 'development'
+const project = process.env.GCLOUD_PROJECT.split('-')[2]
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
@@ -33,8 +33,6 @@ const bazaaar_v1 = new web3.eth.Contract(
 )
 
 exports.order = functions.region('asia-northeast1').https.onCall(async (params, context) => {
-  console.log(process.env.GCLOUD_PROJECT)
-  console.log(process.env.GCLOUD_PROJECT.split('-')[2])
   const data = params.order
   const hash = await bazaaar_v1.methods
     .requireValidOrder_(

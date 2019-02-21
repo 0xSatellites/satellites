1400bc1676782e90b5be57b6f25a347dfe5f0f2d
ookimaki <noreply@ookimaki.com>
2019-02-21 08:32:22 +0000

    add

--- functions/index.js
+++ functions/index.js
@@ -34,8 +34,6 @@ const bazaaar_v1 = new web3.eth.Contract(
 
 exports.order = functions.region('asia-northeast1').https.onCall(async (params, context) => {
   console.log(process.env.GCLOUD_PROJECT)
-  console.log(process.env.project)
-  console.log(functions.config().env.project)
   const data = params.order
   const hash = await bazaaar_v1.methods
     .requireValidOrder_(

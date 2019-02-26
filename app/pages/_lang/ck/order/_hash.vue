a0ddc1b48c02c016a26c52ce4a4f693a9fa18e8e
ookimaki <noreply@ookimaki.com>
2019-02-26 08:26:31 +0000

    #39 fix half

--- app/pages/_lang/ck/order/_hash.vue
+++ app/pages/_lang/ck/order/_hash.vue
@@ -1,63 +1,91 @@
 <template>
-<div>
+  <div>
     <section class="l-information">
-      <div class=l-information__img><img class="ogpimg" :src="order.ogp"></div>
+      <div class="l-information__img">
+        <img class="ogpimg" :src="order.ogp" />
+      </div>
       <div class="l-information__frame" v-if="order.valid">
-        <div class="l-information__name">{{ order.metadata.name}} / Gen.{{ order.metadata.generation}}</div>
-        <div class="l-information__txt"># {{ order.metadata.id}}</div>
-        <div class="l-information__txt">Crypto Kitties</div>
+        <div class="l-information__name">
+          {{ order.metadata.name }}
+        </div>
+        <div class="l-information__txt">#{{ order.metadata.id }}</div>
+        <div class="l-information__txt">CryptoKitties</div>
+        <ul class="l-information__data">
+          <li><span class="l-information__rarity l-item__rarity--5" v-for="(i) in getRarity(order.metadata)" :key="i + '-rarity'">★</span></li>
+        </ul>
+        <ul class="l-information__data">
+          <li><strong>Gen：</strong> {{order.metadata.generation}} </li>
+          <li><strong>Cooldown：</strong> {{coolDownIndexToSpeed(order.metadata.status.cooldown_index)}}</li>
+        </ul>
+        <ul class="l-information__data">
+          <li><span class="l-information__name">Ξ {{ fromWei(order.price) }} ETH</span></li>
+        </ul>
         <v-form v-model="valid" class="center">
-          <v-flex center>
-            <v-checkbox
-              class="center"
-              v-model="checkbox"
-              :rules="[v => !!v || '']"
-              label="利用規約に同意する"
-              required
-            ></v-checkbox>
-          </v-flex>
-        <div class="l-information__action">
-          <v-btn
-          class="l-information__action__btn white_text"
-          color="#3498db"
-          large
-          @click="purchase"
-          :disabled="!checkbox"
-          value="purchase"
-          >購入する
-           <v-progress-circular size=16 class="ma-2" v-if="loading"
-              indeterminate
-            ></v-progress-circular>
-          </v-btn>
-      </div>
-
+          <v-checkbox
+            class="center"
+            v-model="checkbox"
+            :rules="[v => !!v || '']"
+            label="利用規約に同意する"
+            required
+          ></v-checkbox>
+          <div class="l-information__action">
+            <v-btn
+              class="l-information__action__btn white_text"
+              color="#3498db"
+              large
+              @click="purchase"
+              :disabled="!checkbox"
+              value="purchase"
+              >購入する
+              <v-progress-circular
+                size="16"
+                class="ma-2"
+                v-if="loading"
+                indeterminate
+              ></v-progress-circular>
+            </v-btn>
+            <div v-if="order.valid">
+              <a
+                :href="
+                  'https://twitter.com/share?url=https://bazaaar.io/ck/order/' +
+                    order.hash +
+                    '&text=' +
+                    '出品されました! ' +
+                    order.metadata.name +
+                    '/ Gen.' +
+                    order.metadata.generation +
+                    '&hashtags=bazaaar, バザール, CryptoKitties'
+                "
+                class="twitter-share-button"
+                data-size="large"
+                data-show-count="false"
+                target="”_blank”"
+              >
+                <v-icon class="mt-4" color="#3498db">fab fa-twitter</v-icon>
+              </a>
+            </div>
+          </div>
         </v-form>
-      <div class="l-information__action__btn" v-if="order.valid">
-       <a :href="'https://twitter.com/share?url=https://bazaaar.io/ck/order/' + order.hash +
-        '&text=' + '出品されました! '+ order.metadata.name  + '/ Gen.' + order.metadata.generation +
-        '&hashtags=bazaaar, バザール, CryptoKitties'" class="twitter-share-button" data-size="large" data-show-count="false" target=”_blank”>
-        Tweetする
-        </a>
       </div>
-    </div>
-
     </section>
     <section class="c-index c-index--recommend" v-if="recommend.lengh">
-        <h2 class="c-index__title">関連アセット</h2>
-        <ul>
-        <li v-for="(recommend, i ) in recommend" :key="i">
-        <nuxt-link :to="'/ck/order/'+ recommend.hash" class="c-card">
-        <div class="c-card__img"><img :src="recommend.metadata.image_url" alt=""></div>
-        <div class="c-card__name">{{recommend.metadata.name}}</div>
-        <div class="c-card__txt">#{{recommend.id}}</div>
-        <div class="c-card__eth">{{recommend.price / 1000000000000000000}} ETH</div>
-        </nuxt-link>
+      <h2 class="c-index__title">関連アセット</h2>
+      <ul>
+        <li v-for="(recommend, i) in recommend" :key="i">
+          <nuxt-link :to="'/ck/order/' + recommend.hash" class="c-card">
+            <div class="c-card__img">
+              <img :src="recommend.metadata.image_url" alt="" />
+            </div>
+            <div class="c-card__name">{{ recommend.metadata.name }}</div>
+            <div class="c-card__txt">#{{ recommend.id }}</div>
+            <div class="c-card__eth">
+              {{ recommend.price / 1000000000000000000 }} ETH
+            </div>
+          </nuxt-link>
         </li>
-        </ul>
+      </ul>
     </section>
-    <div>
-
-    </div>
+    <div></div>
     <modal
       v-if="modal"
       v-on:closeModal="closeModal"
@@ -71,6 +99,8 @@
 import client from '~/plugins/ethereum-client'
 import firestore from '~/plugins/firestore'
 import Modal from '~/components/modal'
+import kitty from '~/plugins/kitty'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 const config = require('../../../../config.json')
 
@@ -82,27 +112,29 @@ export default {
   head() {
     var order = this.order
     return {
-      meta:
-      [{ hid: 'og:image', property: 'og:image', content: order.ogp }]
+      meta: [{ hid: 'og:image', property: 'og:image', content: order.ogp }]
     }
   },
   data() {
     return {
-      loading:false,
+      loading: false,
       valid: true,
       checkbox: false,
       modal: false,
       modalNo: 4,
-      hash: "",
-      }
+      hash: ''
+    }
   },
   async asyncData({ store, params }) {
     const order = await firestore.doc('order', params.hash)
     await store.dispatch('order/setOrder', order)
 
-    const recommend = await firestore.getRelatedValidOrders(params.hash, order.maker, order.id)
+    const recommend = await firestore.getRelatedValidOrders(
+      params.hash,
+      order.maker,
+      order.id
+    )
     await store.dispatch('order/setOrders', recommend)
-
   },
   mounted: async function() {
     const store = this.$store
@@ -121,11 +153,20 @@ export default {
     order() {
       return this.$store.getters['order/order']
     },
-    recommend(){
+    recommend() {
       return this.$store.getters['order/orders']
     }
   },
   methods: {
+    coolDownIndexToSpeed(index) {
+      return kitty.coolDownIndexToSpeed(index)
+    },
+    getRarity(asset) {
+        return kitty.getRarity(asset)
+    },
+    fromWei(wei) {
+        return client.utils.fromWei(wei)
+    },
     async purchase() {
       const account = this.account
       const order = this.order
@@ -151,7 +192,7 @@ export default {
           ],
           order.v,
           order.r,
-          order.s,
+          order.s
         )
         .send({ from: account.address, value: order.price })
         .on('transactionHash', hash => {
@@ -159,21 +200,21 @@ export default {
           this.hash = hash
           this.modal = true
         })
-        .on('error', err => alert(err));
+        .on('error', err => alert(err))
     },
     closeModal() {
       this.modal = false
-    },
+    }
   }
 }
 </script>
 <style scoped>
 .twitter-share-button {
-text-decoration: none;
-color: white;
+  text-decoration: none;
+  color: white;
 }
 
-.share{
+.share {
   max-width: 100px;
   text-align: center;
   padding: 10px 0;
@@ -184,8 +225,7 @@ color: white;
   color: white;
 }
 
-.v-input__control{
+.v-input__control {
   margin: 0 auto;
 }
 </style>
-
--- app/plugins/kitty.js
+++ app/plugins/kitty.js
@@ -16,9 +16,48 @@ const getKittyById = async id => {
   const result = await instance.get('kitties/'+ id)
   return result.data
 }
+
+const coolDownIndexToSpeed = index => {
+  switch(index) {
+    case 0:
+    return 'Fast'
+    case 1:
+    case 2:
+    return 'Swift'
+    case 3:
+    case 4:
+    return 'Snappy'
+    case 5:
+    case 6:
+    return 'Brisk'
+    case 7:
+    case 8:
+    return 'Plodding'
+    case 9:
+    case 10:
+    return 'Slow'
+    case 11:
+    case 12:
+    return 'Sluggish'
+    case 13:
+    return 'Catatonic'
+    default:
+    return 'unknown'
+  }
+}
+
+const getRarity = kitty => {
+  var rarity = 3
+  if(kitty.is_fancy) rarity++
+  if(kitty.is_exclusive) rarity++
+  return rarity
+}
+
 const kitty = {
+  coolDownIndexToSpeed:coolDownIndexToSpeed,
   getKittiesByWalletAddress: getKittiesByWalletAddress,
-  getKittyById: getKittyById
+  getKittyById: getKittyById,
+  getRarity:getRarity
 }
 
 export default kitty

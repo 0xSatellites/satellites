47e117d92ff1b484a9ced5e2be3906a3c587f411
Daiki Kunii <dai0106ki@gmail.com>
2019-02-28 18:00:53 +0900

    [fix]

--- app/components/header.vue
+++ app/components/header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="l-header">
         <div class="l-header__logo">
-        <nuxt-link :to="$t('header.topJA')">
+        <nuxt-link :to="$t('header.topHD')">
             <img src="~/assets/img/header/logo_pc.svg" alt="bazaaar.io" class="u-obj--pc">
         <img src="~/assets/img/header/logo_sp.svg" alt="" class="u-obj--sp">
         </nuxt-link>
@@ -9,7 +9,7 @@
         <nav class="l-header__nav">
           <ul>
             <li>
-              <nuxt-link :to="$t('header.mypageJA')" class="pa-3">{{$t('header.mypage')}}</nuxt-link>
+              <nuxt-link :to="$t('header.mypageHD')" class="pa-3">{{$t('header.mypage')}}</nuxt-link>
 
               <!-- <nuxt-link :to="'/myitems'" class="pa-1">{{$t('header.howto')}}</nuxt-link> -->
               <NuxtLink v-if="$i18n.locale === 'en'" :to="`/ja` + $route.fullPath" class="ml-2"><img src="~/assets/img/country/japan.png"/></NuxtLink>

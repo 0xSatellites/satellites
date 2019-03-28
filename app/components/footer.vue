e45cd908cf1bed3d0dfae757911fcc16b84d2cb1
ksuhara <ponta.blockchain@gmail.com>
2019-03-28 13:21:51 +0900

    info lang

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -3,9 +3,10 @@
 <footer class="l-footer">
     <div class="l-footer__company">
     <small>&copy; 2019 BlockBase Co.,Ltd.</small>
-        <div><a href="https://block-base.co/" target="_blank">{{$t('footer.company')}}</a>｜
-        <a href="/terms">{{$t('footer.terms')}}</a>｜
-        <a href="/info">{{$t('footer.info')}}</a>｜
+        <div>
+        <a href="https://block-base.co/" target="_blank">{{$t('footer.company')}}</a>｜
+        <nuxt-link :to="$t('footer.termsHD')">{{$t('footer.terms')}}</nuxt-link>｜
+        <nuxt-link :to="$t('footer.infoHD')">{{$t('footer.info')}}</nuxt-link>｜
         <a href="https://twitter.com/bazaaario" target="_blank">{{$t('footer.contact')}}</a>
         </div>
     </div>

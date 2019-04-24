99c35d92a733e8a5dbff17a31adff5a26761318b
ksuhara <ponta.blockchain@gmail.com>
2019-04-24 17:48:42 +0900

    related asset

---
+++ app/components/related.vue
@@ -0,0 +1,79 @@
+<template>
+<ul>
+        <li v-for="(recommend, i) in recommend" :key="i">
+          <nuxt-link v-if="recommend.asset === ck" :to="$t('index.holdLanguageCK') + recommend.hash" class="c-card">
+              <div class="c-card__label c-card__label__rarity--5"><span v-for="(i) in getRarity(recommend)" :key="i + '-rarity'">★</span></div>
+              <div class="c-card__img"><img :src="recommend.metadata.image_url" /></div>
+              <div class="c-card__name" v-if="recommend.metadata.name">{{ recommend.metadata.name.substring(0,25) }}</div>
+              <div class="c-card__name" v-else>Gonbee</div>
+              <div class="c-card__txt"># {{ recommend.id }}</div>
+              <div class="c-card__txt">Gen {{recommend.metadata.generation}} : {{coolDownIndexToSpeed(recommend.metadata.status.cooldown_index)}}</div>
+              <div class="c-card__eth">Ξ {{ fromWei(recommend.price) }} ETH</div>
+          </nuxt-link>
+          <nuxt-link v-else-if="recommend.asset === ctn" :to="$t('index.holdLanguageCTN') + recommend.hash" class="c-card">
+              <div class="c-card__label c-card__label__rarity--5"><span v-for="(i) in getRarity(recommend)" :key="i + '-rarity'">★</span></div>
+              <div class="c-card__img"><img :src="recommend.metadata.image_url" /></div>
+              <div class="c-card__name" v-if="recommend.metadata.name">{{ recommend.metadata.name.substring(0,25) }}</div>
+              <div class="c-card__name" v-else>Gonbee</div>
+              <div class="c-card__txt"># {{ recommend.id }}</div>
+              <div class="c-card__txt">Gen {{recommend.metadata.generation}} : {{coolDownIndexToSpeed(Number(recommend.metadata.status.cooldown_index))}}</div>
+              <div class="c-card__eth">Ξ {{ fromWei(recommend.price) }} ETH</div>
+          </nuxt-link>
+          <nuxt-link v-else-if="recommend.asset === mchh" :to="$t('index.holdLanguageMCHH') + recommend.hash" class="c-card">
+              <div class="c-card__label c-card__label__rarity--5"><span v-for="(i) in getRarity(recommend)" :key="i + '-rarity'">★</span></div>
+              <div class="c-card__img"><img class="pa-4" :src="recommend.metadata.image_url" /></div>
+              <div class="c-card__name" v-if="recommend.metadata.attributes.hero_name">{{ recommend.metadata.attributes.hero_name.substring(0,25) }}</div>
+              <div class="c-card__name" v-else>Gonbee</div>
+              <div class="c-card__txt"># {{ recommend.id }}</div>
+              <div class="c-card__txt">Lv. {{recommend.metadata.attributes.lv}} </div>
+              <div class="c-card__eth">Ξ {{ fromWei(recommend.price) }} ETH</div>
+          </nuxt-link>
+          <nuxt-link v-else-if="recommend.asset === mche" :to="$t('index.holdLanguageMCHE') + recommend.hash" class="c-card">
+              <div class="c-card__label c-card__label__rarity--5"><span v-for="(i) in getRarity(recommend)" :key="i + '-rarity'">★</span></div>
+              <div class="c-card__img"><img class="pa-4" :src="recommend.metadata.image_url" /></div>
+              <div class="c-card__name" v-if="recommend.metadata.attributes.extension_name">{{ recommend.metadata.attributes.extension_name.substring(0,25) }}</div>
+              <div class="c-card__name" v-else>Gonbee</div>
+              <div class="c-card__txt"># {{ recommend.id }}</div>
+              <div class="c-card__txt">Lv. {{recommend.metadata.attributes.lv}} </div>
+              <div class="c-card__eth">Ξ {{ fromWei(recommend.price) }} ETH</div>
+          </nuxt-link>
+        </li>
+      </ul>
+</template>
+
+<script>
+import client from '~/plugins/ethereum-client'
+import common from '~/plugins/common'
+import kitty from '~/plugins/kitty'
+
+const config = require('../config.json')
+const project = process.env.project
+const host = config.host[project]
+const ck = config.contract[project].ck
+const ctn = config.contract[project].ctn
+const mchh = config.contract[project].mchh
+const mche = config.contract[project].mche
+
+export default {
+    props: ['asset', 'recommend'],
+    methods: {
+    getRarity(asset) {
+      return common.getRarity(asset)
+    },
+    fromWei(wei) {
+      return client.utils.fromWei(wei)
+    },
+    coolDownIndexToSpeed(index) {
+      return kitty.coolDownIndexToSpeed(index)
+    },
+    },
+    data() {
+        return{
+            ck,
+            ctn,
+            mchh,
+            mche,
+         }
+     }
+}
+</script>

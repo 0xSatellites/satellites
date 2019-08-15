389039b9047a8562ac6e397f3a97beb4149fe0e9
Kohei Tanaka <kohei69@gmail.com>
2019-08-15 17:33:43 +0900

    add index readme

--- templates/pages/index.vue
+++ templates/pages/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <v-content>
-    <img src="@/assets/img/top_visual/satellites.png" alt="" style="width:100%" />
-  </v-content>
+  <section class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        <img src="@/assets/img/top_visual/satellites.png" alt="" style="width:100%" />
+      </h1>
+      <div class="links">
+        <a href="https://github.com/block-base/satellites" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -11,4 +19,41 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class Index extends Vue {}
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
+    Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
